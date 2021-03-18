7b6279bbbc1e26c7dd9788430cb507b143586e01
LoJones <lmjones1@lakeheadu.ca>
2021-03-18 02:02:57 +0000

    adding forms!

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,3 +5,13 @@ class User < ActiveRecord::Base
   has_many :likes
 
 end
+class User < ActiveRecord::Base
+
+  # ... The rest of your code is still here ...
+
+  # --- Add these lines ---
+  validates :email, :username, uniqueness: true
+  validates :email, :avatar_url, :username, :password, presence: true
+  # -----------------------
+
+end
